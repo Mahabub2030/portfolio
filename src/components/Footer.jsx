4bd6ebf6bb4739d5881364f6514690f85ce248a6
Mahaub Alam <mahabubalom72091@gmail.com>
2025-04-17 19:48:38 +0300

    added some text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-4 text-center">
-      <p>&copy; {new Date().getFullYear()} Rohit Singh. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Mahabub_Alam. All rights reserved.</p>
     </footer>
   );
 };
